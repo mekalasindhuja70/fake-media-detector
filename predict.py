b96bab15b1d9cd9848e0d0adbb97c19283aa2845
mekalasindhuja70 <mekalasindhuja70@gmail.com>
2026-07-16 11:50:44 +0530

    Update model path for deployment

--- predict.py
+++ predict.py
@@ -5,13 +5,21 @@ from tensorflow.keras.models import load_model
 
 from gradcam import generate_gradcam
 
+
 # ==========================================
 # Load Model
 # ==========================================
 
-MODEL_PATH = "model/fake_media_detector_finetuned.keras"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+MODEL_PATH = os.path.join(
+    BASE_DIR,
+    "model",
+    "fake_media_detector_finetuned.keras"
+)
 
 model = load_model(MODEL_PATH)
+
 
 # ==========================================
 # Class Labels
@@ -21,6 +29,7 @@ CLASSES = [
     "Fake (AI Generated)",
     "Real Human"
 ]
+
 
 # ==========================================
 # Predict Function
@@ -34,12 +43,16 @@ def predict_image(image_path):
 
     img = tf.keras.utils.load_img(
         image_path,
-        target_size=(224,224)
+        target_size=(224, 224)
     )
 
     img_array = tf.keras.utils.img_to_array(img)
 
-    img_array = np.expand_dims(img_array, axis=0)
+    img_array = np.expand_dims(
+        img_array,
+        axis=0
+    )
+
 
     # -------------------------------
     # Prediction
@@ -50,9 +63,12 @@ def predict_image(image_path):
         verbose=0
     )
 
-    predicted_index = np.argmax(prediction[0])
+    predicted_index = np.argmax(
+        prediction[0]
+    )
 
     predicted_class = CLASSES[predicted_index]
+
 
     # -------------------------------
     # Detection Status
@@ -78,25 +94,33 @@ def predict_image(image_path):
             "Possible AI-generated artifacts"
         ]
 
+
     # -------------------------------
     # Heatmap Path
     # -------------------------------
 
     filename = os.path.basename(image_path)
 
-    heatmap_name = "heatmap_" + filename
+    heatmap_name = (
+        "heatmap_" + filename
+    )
 
     heatmap_path = os.path.join(
+        BASE_DIR,
         "static",
         "heatmaps",
         heatmap_name
     )
 
-     #generate_gradcam(
-      #  model,
-      #  image_path,
-     #   heatmap_path
-    #)
+
+    # Uncomment when GradCAM is enabled
+    #
+    # generate_gradcam(
+    #     model,
+    #     image_path,
+    #     heatmap_path
+    # )
+
 
     # -------------------------------
     # Return Results
